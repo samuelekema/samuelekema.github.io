29706fd927828183bcc7e3302c48c3e9d6778094
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2023-05-16 19:59:17 -0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,1 @@
+document.getElementById("hook-span").style.color = "red";
---
+++ js.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$('a[href^="#container-2"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+$('a[href^="#container-3"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+    
+$('a[href^="#container-4"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+  
+});
