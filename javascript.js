a1566b292488c920cda4d1d865d336b622a14c66
samuelekema <100537240+samuelekema@users.noreply.github.com>
2023-05-17 21:33:13 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 document.getElementById("hook-span").style.color = "red";
+document.getElementById("service").addEventListener("click", mouseClicked);
+document.getElementById("contact").addEventListener("click", mouseClicked);
+document.getElementById("about-us").addEventListener("click", mouseClicked);
+
+function mouseClicked(){
+    alert("This function is not yet available. Check back later");
+}
